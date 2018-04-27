1d75a09c5629b011630b5f7821b80d6d37437705
Thomas Ingram <ithomas@netapp.com>
2018-04-27 11:37:26 -0400

    Enable tab completion for `c`

--- fish/functions/c.fish
+++ fish/functions/c.fish
@@ -2,4 +2,4 @@ function c --wraps=(cd ~/Code)
 	# builtin cd ~/Code/$argv
 	cd ~/Code/$argv
 end
-# complete --command c --arguments '(__fish_complete_directories ~/Code/)'
+complete --command c --arguments '(__fish_complete_directories ~/Code/)'
